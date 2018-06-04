a9b67ca6855e094f1dab1ff76cd8c4991a729d68
Jonathan Neal <jonathantneal@hotmail.com>
2018-06-04 15:08:52 -0400

    Update project configuration

--- .rollup.js
+++ .rollup.js
@@ -1,11 +1,10 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-	name: 'focusWithin',
 	input: 'index.js',
 	output: [
 		{ file: 'index.es.js', format: 'es' },
-		{ file: 'index.umd.js', format: 'umd' }
+		{ file: 'index.cjs.js', format: 'cjs' }
 	],
 	plugins: [
 		babel({
